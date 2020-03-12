8a235c0355ac2851fa2baf5da80a1b1480ba3710
ggace15 <61539048+ggace15@users.noreply.github.com>
2020-03-12 15:34:16 +0900

    first

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+#!C:\Users\KAIST\AppData\Local\Programs\Python\Python38-32\python.exe
+print("content-type: text/html; charset=utf-8\n")
+
+b=1
+
+print(f'''<DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+    <body>
+        <img src="post_landmark.jpg">
+        <h1> (3+4+5)/3 = <h1>
+        <h1>{b}</h1>
+    </body>
+</html>''')
